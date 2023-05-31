2a4a9e8cbdf6fbafcf559ff742ae71cbe41ae284
Gabriel Soares <gabrielsoaresm94@gmail.com>
2023-05-31 02:44:13 -0300

    refactor: changes code to fit an architecture of modules in intentions

--- intentions/src/modules/intentions/controllers.py
+++ intentions/src/modules/intentions/controllers.py
@@ -1,26 +1,21 @@
-from flask import Flask
-from flask import jsonify
+
+from flask import Blueprint, jsonify
 from flask_restx import Api, Resource
 
-app = Flask(__name__)
+intentions_module = Blueprint('/', __name__)
 api = Api(
-  app,
+  intentions_module,
   version='1.0',
   title='Intentions Service API',
   description='A service of purchase intentions',
 )
 
-# ns = api.namespace('intentions')
+ns = api.namespace('intentions')
 
-@api.route('/v1/hello')
+@ns.route('/')
 class HelloWorld(Resource):
   def get(self):
     data = {
       'message': 'Hello World!',
     }
     return jsonify(data)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
