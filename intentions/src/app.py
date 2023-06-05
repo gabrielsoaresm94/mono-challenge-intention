d18625dac7baa5a9ba63af1890b252b2434f4d06
Gabriel Soares <gabrielsoaresm94@gmail.com>
2023-06-05 00:48:02 -0300

    feat(intentions): try to run migrations when build the service

--- intentions/src/app.py
+++ intentions/src/app.py
@@ -39,7 +39,7 @@ blueprint = Blueprint("/docs/", __name__)
 api = Api(
   blueprint,
   doc='/docs/',
-  version='1.0',
+  version='1.0.0',
   title='Intentions Service API',
   description='A service of purchase intentions'
 )
